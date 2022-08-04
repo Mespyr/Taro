d94f6fedacea97a00d0d0985cab120704a69936f
Mespyr <senukcw@gmail.com>
2022-08-03 21:32:31 -0400

    make code get compiled to an executable

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,8 @@
 #include "include/parser.h"
 #include "include/compiler.h"
 
+#define exit_on_error(exit_code) if (exit_code != 0) exit(exit_code);
+
 int main(int argc, char* argv[])
 {
 	if (argc < 2) 
@@ -15,9 +17,17 @@ int main(int argc, char* argv[])
 		return 1;
 	}
 
+	std::cout << "[note] tokenizing file" << std::endl;
 	std::vector<Token> tokens = tokenize_file(argv[1]);
+
+	std::cout << "[note] parsing tokens" << std::endl;
 	Program program = parse_tokens(tokens);
+
+	std::cout << "[note] compiling to assembly" << std::endl;
 	compile_to_asm(program, "out.asm");
+
+	std::cout << "[cmd] fasm out.asm" << std::endl;
+	exit_on_error(std::system("fasm out.asm"));	
 
 	return 0;
 }
