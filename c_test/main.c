4c62e750cdf667c56801954e5c4f36448a80ce20
Mespyr <senukcw@gmail.com>
2024-03-31 16:04:48 -0400

    update main.c

--- c_test/main.c
+++ c_test/main.c
@@ -10,9 +10,8 @@ typedef U8* String;
 typedef union {
     F32 _F32;
     F64 _F64;
-    I64 pass;
+    void* pass;
 } FloatPasser;
-FloatPasser OCH_float;
 void* OCH_stack[1024];
 U32 OCH_top = 0;
 
@@ -30,23 +29,21 @@ extern F32 distance(Vec2*, Vec2*);
 
 /* ######### functions ############ */
 void helloworld() {
-	void *temp;
     OCH_stack[OCH_top++] = (void*)"Hello World!\n";  // PUSH String
-	temp = OCH_stack[--OCH_top];
+	void *temp = OCH_stack[--OCH_top];
 	print((String)temp);
 }
 
 int main() {
-    void *temp, *temp1;
-
     OCH_stack[OCH_top++] = (void*)20;  // PUSH I64 20
-    temp = OCH_stack[--OCH_top];       // POP to temp
+    void *temp = OCH_stack[--OCH_top];       // POP to temp
     PUTU((I64)temp);                   // CALL_EXTERNAL PUTU
 
-    OCH_float._F64 = 12.3;  // PUSH F64 12.3
-    OCH_stack[OCH_top++] = (void*)OCH_float.pass;
-    OCH_float.pass = (I64)OCH_stack[--OCH_top];  // POP to global float pass
-    PUTD(OCH_float._F64); // CALL_EXTERNAL PUTD
+    FloatPasser float_temp;  // PUSH F64 12.3
+	float_temp._F64 = 12.3;
+    OCH_stack[OCH_top++] = float_temp.pass;
+    float_temp.pass = OCH_stack[--OCH_top];  // POP to float temp
+    PUTD(float_temp._F64); // CALL_EXTERNAL PUTD
 
     OCH_stack[OCH_top++] = (void*)helloworld; // PUSHFN print
     ((void (*)(void))(OCH_stack[--OCH_top]))(); // CALLFN
@@ -58,19 +55,18 @@ int main() {
 	Vec2 Vec2_temp1 = {4.2, 6.9};
     OCH_stack[OCH_top++] = (void*)&Vec2_temp1;
 	// bind p1 p2
-	void *p1, *p2;
-    p2 = OCH_stack[--OCH_top];
-    p1 = OCH_stack[--OCH_top];
+    void *bind_p2 = OCH_stack[--OCH_top];
+    void *bind_p1 = OCH_stack[--OCH_top];
 
-    OCH_stack[OCH_top++] = p1; // PUSH p1
-    OCH_stack[OCH_top++] = p2; // PUSH p2
+    OCH_stack[OCH_top++] = bind_p1; // PUSH p1
+    OCH_stack[OCH_top++] = bind_p2; // PUSH p2
 
     temp = OCH_stack[--OCH_top]; // POP to temp
-    temp1 = OCH_stack[--OCH_top]; // POP to temp1
-	OCH_float._F32 = distance((Vec2*)temp1, (Vec2*)temp); // CALL-EXTERNAL distance
-    OCH_stack[OCH_top++] = (void*)OCH_float.pass; // PUSH result
-	OCH_float.pass = (I64)OCH_stack[--OCH_top];  // POP to global float pass
-    PUTD(OCH_float._F32); // CALL_EXTERNAL PUTD
+    void *temp1 = OCH_stack[--OCH_top]; // POP to temp1
+	float_temp._F32 = distance((Vec2*)temp1, (Vec2*)temp); // CALL-EXTERNAL distance
+    OCH_stack[OCH_top++] = float_temp.pass; // PUSH result
+	float_temp.pass = OCH_stack[--OCH_top];  // POP to global float pass
+    PUTD(float_temp._F32); // CALL_EXTERNAL PUTD
 
     return 0;
 }
